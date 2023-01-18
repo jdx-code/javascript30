da112797588395b89fe63e968947f7c79d185cd9
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-18 13:30:27 +0530

    javascript file added

---
+++ javascriptDrumkit/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', (e) => {
+    // Select the audio associated with the key
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    // Select the particular class(or div) associated with the key
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return // stops the function from running altogether
+    audio.currentTime = 0 // rewind to the start
+    audio.play();
+    key.classList.add('playing');
+});
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; //skip it if it is not a tranform
+
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
