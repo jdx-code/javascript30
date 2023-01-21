92fa35bee01e8d8947325836fb003a5b8da5b77d
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-01-21 19:25:29 +0530

    Fixing javascript functions

--- javascriptDrumkit/js/main.js
+++ javascriptDrumkit/js/main.js
@@ -1,27 +1,33 @@
+function playAudio(audio, key) {
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function audioOnMouseClick () {
+    const audio = document.querySelector(`audio[data-key="${this.getAttribute('data-key')}"]`);
+    const key = document.querySelector(`.key[data-key="${this.getAttribute('data-key')}"]`);
+    //console.log(a);
+    if(!audio) return;
+    playAudio(audio, key);    
+}
+
 document.querySelectorAll('.key').forEach(function(key) {
-    key.addEventListener('click', function(){
-        let a = document.querySelector(`audio[data-key="${this.getAttribute('data-key')}"]`);
-        let k = document.querySelector(`.key[data-key="${this.getAttribute('data-key')}"]`);
-        //console.log(a);
-        if(!a) return;
-        a.currentTime = 0;
-        a.play();
-        k.classList.add('playing');
-    });
+    key.addEventListener('click', audioOnMouseClick);
 });
 
 window.addEventListener('keydown', (e) => {
     // console log the event
-    console.log(e);
+    // console.log(e);
+
     // Select the audio associated with the key
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
     // Select the particular class(or div) associated with the key
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
     if(!audio) return // stops the function from running altogether
-    audio.currentTime = 0 // rewind to the start
-    audio.play();
-    key.classList.add('playing');
+    playAudio(audio, key);    
 });
 
 function removeTransition(e) {
